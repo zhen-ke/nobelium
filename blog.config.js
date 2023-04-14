0555a3988abbda325beed5f3101424205b0f47b3
XMit <xmit0806@outlook.com>
2023-04-14 09:33:51 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 const BLOG = {
   title: 'XMit',
   author: 'jackWang',
-  email: 'mail@2020369.xyz',
+  email: 'se7en56@qq.com',
   link: 'https://xmit.vercel.app',
   description: 'Frontend Engineer.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: 'https://cdn.jsdelivr.net/gh/alist-org/logo@main/logo.svg',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
